6c86ed6419d11a838b3aba2de4cc0938b6014211
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2021-10-20 17:20:16 -0400

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -1,9 +1,7 @@
-// 2021 FALL CS 445 LAB #3  STUDENT STARTER FILE
-
 import java.io.*;
 import java.util.*;
 
-public class LinkedList<T>
+public class LinkedList<T extends Comparable>
 {
 	private Node<T> head;  // pointer to the front (first) element of the list
 
@@ -12,38 +10,90 @@ public class LinkedList<T>
 		head = null; // compiler does this anyway. just for emphasis
 	}
 
-	// COPY ALL NODES FROM OTHER LIST INTO THIS LIST. WHEN COMPLETED THIS LIST IDENTICAL TO OTHER
+	// LOAD LINKED LIST FROM INCOMING FILE
 	@SuppressWarnings("unchecked")
-	public LinkedList( LinkedList<T> other )
-	{
-		for (Node curr = other.head; curr != null; curr = curr.getNext())
-			insertAtTail((T)curr.getData());
+	public LinkedList( String fileName, boolean orderedFlag )
+	{	head = null;
+		try
+		{
+			BufferedReader infile = new BufferedReader( new FileReader( fileName ) );
+			while ( infile.ready() )
+			{
+				if (orderedFlag)
+					insertInOrder( (T)infile.readLine() );  // WILL INSERT EACH ELEM INTO IT'S SORTED POSITION
+				else
+					insertAtTail( (T)infile.readLine() );  // TACK EVERY NEWELEM ONTO END OF LIST. ORIGINAL ORDER PRESERVED
+			}
+			infile.close();
+		}
+		catch( Exception e )
+		{
+			System.out.println( "FATAL ERROR CAUGHT IN C'TOR: " + e );
+			System.exit(0);
+		}
 	}
 
-	// INSERTS NEW NODE AT FRONT PUSHING EXISTING NODES BACK ONE PLACE
+	//-------------------------------------------------------------
+
+	// inserts new elem at front of list - pushing old elements back one place
 	public void insertAtFront(T data)
 	{
 		head = new Node<T>(data,head);
 	}
 
-	// USE THE TOSTRING AS OUR PRINT
+	// we use toString as our print
 	public String toString()
 	{
 		String toString = "";
 
-		for (Node<T> curr = head; curr != null; curr = curr.getNext())
+		for (Node<T> curr = head; curr != null; curr = curr.next )
 		{
-			toString += curr.getData();		// WE ASSUME OUR T TYPE HAS toString() DEFINED
-			if (curr.getNext() != null)
-				toString += " -> ";
+			toString += curr.data;		// WE ASSUME OUR T TYPE HAS toString() DEFINED
+			if (curr.next != null)
+				toString += " ";
 		}
 
-		return toString + "\n";
+		return toString;
 	}
 
 	// ########################## Y O U   W R I T E    T H E S E    M E T H O D S ########################
 
-	// TACK A NEW NODE ONTO THE END (CABOOSE) OF THE LIST
+
+
+	public int size() // OF COURSE MORE EFFICIENT to KEEP COUNTER BUT YOU  MUST WRITE LOOP
+	{
+		int count = 0;
+
+		for (Node curr = head; curr != null; curr = curr.next)
+			count++;
+
+		return count;
+	}
+
+	public boolean empty()
+	{
+		return size() == 0;
+	}
+
+	public boolean contains( T key )
+	{
+		return search(key) != null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public Node<T> search( T key )
+	{
+		if (head == null) return null;
+
+		for (Node curr = head; curr != null; curr = curr.next) {
+			if (key.equals(curr.data))
+				return curr;
+		}
+
+		return null;
+	}
+
+	// TACK A NEW NODE (CABOOSE) ONTO THE END OF THE LIST
 	@SuppressWarnings("unchecked")
 	public void insertAtTail(T data)
 	{
@@ -54,45 +104,185 @@ public class LinkedList<T>
 
 		Node curr = head;
 
-		while(curr.getNext() != null)
-			curr = curr.getNext();
+		while(curr.next != null)
+			curr = curr.next;
 
-		curr.setNext(new Node<T>(data, null));
+		curr.next = new Node<T>(data, null);
 	}
 
-	// OF COURSE MORE EFFICIENT TO KEEP INTERNAL COUNTER BUT WE MAKE YOU 
-	// COMPUTE IT DYNAMICALLY WITH A TRAVERSAL LOOP
-	public int size()
+	@SuppressWarnings("unchecked")  //CAST TO COMPARABLE THROWS WARNING
+	public void insertInOrder(T  data)
 	{
-		int count = 0;
+		Node curr = head;
 
-		for (Node curr = head; curr != null; curr = curr.getNext())
-			count++;
-
-		return count; 
-	}
-	
-	// MUST USE search() METHOD IN THIS CODE TO DETERMINE THE RETURN VALUE
-	// NO LOOPS ALLOWED 
-	public boolean contains( T key )
-	{
-		return search(key) != null;
-	}
-
-	// TRAVERSE LIST FRONT TO BACK LOOKING FOR THIS DATA VALUE.
-	// RETURN REF TO THE FIRST NODE THAT CONTAINS THIS KEY. 
-	// DO NOT- RETURN REF TO KEY ISIDE NODE
-	// RETURN NULL IF NOT FOUND
-	@SuppressWarnings("unchecked")
-	public Node<T> search( T key )
-	{
-		if (head == null) return null;
-
-		for (Node curr = head; curr != null; curr = curr.getNext()) {
-			if (key.equals(curr.getData()))
-				return curr;
+		if (head == null) { 
+			insertAtFront(data);
+			return;
 		}
 
-		return null;
+		if (size() == 1) {
+			if (data.compareTo(curr.data) < 0) {
+				insertAtFront(data);
+				return;
+			}
+			else {
+				insertAtTail(data);
+				return;
+			}
+		}
+
+		if (data.compareTo(curr.data) < 0) {
+			insertAtFront(data);
+			return;
+		}
+
+		while (curr.next != null) {
+			if (data.compareTo(curr.next.data) > 0)
+				curr = curr.next;
+			else if (data.compareTo(curr.next.data) < 0) {
+				curr.next = new Node<T>(data, curr.next);
+				return;
+			}
+		}
+
+		insertAtTail(data);
 	}
-} //END OF LINKEDLIST CLASS DEFINITION
+
+	@SuppressWarnings("unchecked")
+	public boolean remove(T key)
+	{
+		if (empty()) return false;
+
+		Node curr = head;
+
+		if (curr.data.equals(key)) return removeAtFront();
+
+		while (curr.next != null) {
+			if (curr.next.data.equals(key)) {
+				curr.next = curr.next.next;
+				return true;
+			}
+
+			curr = curr.next;
+		}
+
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	public boolean removeAtTail()	// RETURNS TRUE IF THERE WAS NODE TO REMOVE
+	{
+		if (empty()) return false;
+		if (head.next == null) return removeAtFront();
+
+		Node curr = head;
+
+		while (curr.next.next != null)
+			curr = curr.next;
+
+		curr.next = null;
+		return true;		
+	}
+
+	public boolean removeAtFront() // RETURNS TRUE IF THERE WAS NODE TO REMOVE
+	{
+		if (empty()) return false;
+
+		head = head.next;
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	public LinkedList<T> union( LinkedList<T> other )
+	{
+		LinkedList<T> unionResult = new LinkedList<T>();
+
+		for (Node<T> curr = head; curr != null; curr = curr.next) {
+			unionResult.insertAtTail(curr.data);
+		}
+
+		for (Node<T> curr = other.head; curr != null; curr = curr.next) {
+			if (!unionResult.contains(curr.data)) 
+				unionResult.insertInOrder(curr.data);
+		}
+
+		return unionResult;
+	}
+
+	@SuppressWarnings("unchecked")
+	public LinkedList<T> inter( LinkedList<T> other )
+	{
+		LinkedList<T> inter = new LinkedList<T>();
+
+		for (Node<T> curr = head; curr != null; curr = curr.next) {
+			if (other.contains(curr.data))
+				inter.insertInOrder(curr.data);
+		}
+
+		return inter;
+	}
+	public LinkedList<T> diff( LinkedList<T> other )
+	{
+		LinkedList<T> diff = new LinkedList<T>();
+
+		for (Node<T> curr = head; curr != null; curr = curr.next) {
+			if (!other.contains(curr.data))
+				diff.insertInOrder(curr.data);
+		}
+
+		return diff;
+	}
+	public LinkedList<T> xor( LinkedList<T> other )
+	{
+		return union(other).diff(inter(other)); 
+
+	}
+
+} //END LINKEDLIST CLASS
+
+class Node<T extends Comparable>
+{
+  T data;
+  Node<T> next;
+
+  Node()
+  {
+    this( null, null );
+  }
+
+  Node(T data)
+  {
+    this( data, null );
+  }
+
+  Node(T data, Node<T> next)
+  {
+    setData( data );
+    setNext( next );
+  }
+
+  T getData()
+  {
+    return data;
+  }
+
+  Node<T> getNext()
+  {
+    return next;
+  }
+
+  void setData(T data)
+  {
+     this.data = data;
+  }
+
+  void setNext(Node<T> next)
+  {
+    this.next = next;
+  }
+  public String toString()
+  {
+	  return ""+getData();
+  } 
+	 
+} //EOF
